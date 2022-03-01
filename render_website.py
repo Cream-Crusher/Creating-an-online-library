bdcb1f023e5e655058bbfc7af32a1116190f01ad
ruk228 <icecreamcrusher21@gmail.com>
2022-03-01 21:02:50 +0400

    fix
    1) changed name
    2)linked the numbers to the code
    3)deleted the words parasites

--- render_website.py
+++ render_website.py
@@ -4,32 +4,32 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from more_itertools import chunked
 
 
-def get_information_for_template():
-    name_file = 'book_page_information.json'
+def get_processed_books_and_template():
+    file_name = 'book_page_information.json'
 
-    with open('site-example\statics\{}'.format(name_file), 'r', encoding='utf-8') as my_file:
+    with open('site-example\statics\{}'.format(file_name), 'r', encoding='utf-8') as my_file:
         books_information = json.load(my_file)
 
     env = Environment(
         loader=jinja2.FileSystemLoader('site-example/statics/templates'),
         autoescape=select_autoescape(['html', 'xml'])
     )
-
+    books_num = 10
     template = env.get_template('template.html')
-    processed_books_information = list(chunked(books_information, 10))
-    return template, processed_books_information
+    processed_books = list(chunked(books_information, books_num))
+    return template, processed_books
 
 
 def get_rendered_page():
-    template, processed_books_information = get_information_for_template()
-    number_pages = len(processed_books_information)
+    template, processed_books = get_processed_books_and_template()
+    pages_number = len(processed_books)
 
-    for number_page, books_information in enumerate(processed_books_information, 1):
+    for page_number, books_information in enumerate(processed_books, 1):
         rendered_page = template.render(
             books_information=list(chunked(books_information, 2)),
-            number_page=number_page,
-            number_pages=number_pages
+            page_number=page_number,
+            pages_number=pages_number
             )
 
-        with open(('site-example/pages/index{}.html').format(number_page), 'w', encoding="utf8") as file:
+        with open(('site-example/pages/index{}.html').format(page_number), 'w', encoding="utf8") as file:
             file.write(rendered_page)
