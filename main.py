2e213152f4437eed0663ecdde969ddcec8943164
ruk228 <icecreamcrusher21@gmail.com>
2022-03-22 20:49:57 +0300

    fix main.py

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from livereload import Server
 from render_website import get_rendered_page
 
 
-def rebuild():
-    print("Site rebuilt")
-
 server = Server()
-server.watch('site-example/statics/templates/template.html', get_rendered_page, rebuild())
+server.watch('site-example/statics/templates/template.html', get_rendered_page)
+
 server.serve(root='.')
