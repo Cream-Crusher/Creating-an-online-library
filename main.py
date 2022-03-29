90ade47de0ff1de151cab14330b041cf30f76dff
ruk228 <icecreamcrusher21@gmail.com>
2022-03-30 00:02:15 +0300

    slight changes

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import os
 from livereload import Server
 from render_website import get_rendered_page
 
 
 server = Server()
-server.watch('site-example/statics/templates/template.html', get_rendered_page)
+server.watch(os.path.join('site-example', 'statics', 'templates', 'template.html'), get_rendered_page)
 
 server.serve(root='.')
--- render_website.py
+++ render_website.py
@@ -1,38 +1,43 @@
+import os
 import json
 import jinja2
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from more_itertools import chunked
 
 
-def get_processed_books_and_template():
+def get_processed_books(num = 10):
     file_name = 'book_page_information.json'
+    
+    with open(os.path.join('site-example', 'statics', '{}'.format(file_name)), 'r', encoding='utf-8') as file:
+        books = json.load(file)
 
-    with open('site-example/statics/{}'.format(file_name), 'r', encoding='utf-8') as my_file:
-        books_information = json.load(my_file)
+    processed_books = list(chunked(books, num))
+    return processed_books
 
+
+def get_template():
     env = Environment(
-        loader=jinja2.FileSystemLoader('site-example/statics/templates'),
+        loader=jinja2.FileSystemLoader(os.path.join('site-example', 'statics', 'templates')),
         autoescape=select_autoescape(['html', 'xml'])
     )
-    books_num = 10
     template = env.get_template('template.html')
-    processed_books = list(chunked(books_information, books_num))
-    return template, processed_books
+    return template
 
 
 def get_rendered_page():
-    template, processed_books = get_processed_books_and_template()
+    template = get_template()
+    processed_books = get_processed_books()
     pages_number = len(processed_books)
 
-    for page_number, books_information in enumerate(processed_books, 1):
+    for page_number, books in enumerate(processed_books, 1):
 
         rendered_page = template.render(
-            books_information=list(chunked(books_information, 2)),
+            books=list(chunked(books, 2)),
             page_number=page_number,
             pages_number=pages_number,
             )
 
-        with open(('site-example/pages/index{}.html').format(page_number), 'w', encoding="utf8") as file:
+        with open(os.path.join('site-example', 'pages', 'index{}.html'.format(page_number)), 'w', encoding="utf8") as file:
             file.write(rendered_page)
 
     print("Site rebuilt")
