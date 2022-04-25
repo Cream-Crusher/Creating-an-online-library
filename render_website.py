47328cdb09f33b0178dd3189b8d71560b9b9f1fe
ruk228 <icecreamcrusher21@gmail.com>
2022-04-25 16:58:18 +0300

    changed name and fix flake8

--- render_website.py
+++ render_website.py
@@ -1,24 +1,24 @@
 import os
 import json
-import jinja2
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from more_itertools import chunked
 
 
-def get_processed_books(maximum_number_books_page = 10):
+def get_processed_books():
+    maximum_page_books_number = 10
     file_name = 'book_page_information.json'
-    path_a_file=os.path.join('site-example', 'media', file_name)
+    path_a_file = os.path.join('site-example', 'media', file_name)
 
     with open(path_a_file, 'r', encoding='utf-8') as file:
         books = json.load(file)
 
-    processed_books = list(chunked(books, maximum_number_books_page))
+    processed_books = list(chunked(books, maximum_page_books_number))
     return processed_books
 
 
 def get_template():
     env = Environment(
-        loader=jinja2.FileSystemLoader(os.path.join('site-example', 'templates')),
+        loader=FileSystemLoader(os.path.join('site-example', 'templates')),
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('template.html')
@@ -36,7 +36,7 @@ def rendered_pages():
             books=list(chunked(books, 2)),
             page_number=page_number,
             pages_number=pages_number,
-            )
+        )
         path_a_file = os.path.join('site-example', 'pages', 'index{}.html'.format(page_number))
 
         with open(path_a_file, 'w', encoding="utf8") as file:
